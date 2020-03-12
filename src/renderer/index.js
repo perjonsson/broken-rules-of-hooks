63118dc033a5eab36a26d0bd7e661e9b0884179d
Per Jonsson <per.d.jonsson@gmail.com>
2020-03-12 01:02:01 +0100

    Add react-tiny-link

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { ReactTinyLink } from 'react-tiny-link'
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <ReactTinyLink
+    cardSize="small"
+    showGraphic={true}
+    maxLine={2}
+    minLine={1}
+    url="https://www.youtube.com/watch?v=xn2A_dXoIAA"
+  />,
   document.getElementById('app')
 );
